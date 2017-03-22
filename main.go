cc5d0ffac325335b961722f9df7a110146002d11
tushargarg <tkg.1996@gmail.com>
2017-03-22 15:52:18 +0530

    Updated authentication

--- main.go
+++ main.go
@@ -6,15 +6,19 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"io/ioutil"
+	Password "password"
 
+	"github.com/dgrijalva/jwt-go"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/julienschmidt/httprouter"
 	"github.com/rs/cors"
 )
 
 var database *sql.DB
+var secretKey = []byte("5gjZ7caBbn58J0MqSA3tBiR3OTNTnIKjzIizDJkHTELz8yo4IHyOuUBUUWTkeJ6g7mRjO7Idl98JElbukkYiUOSR7RcJuavxEW7G")
 
 type Signin struct {
 	Username string `json:"username"`
@@ -22,16 +26,22 @@ type Signin struct {
 }
 
 type Response struct {
-	Message string `json:"message"`
-	Token   string `json:"token"`
+	Message  string `json:"message"`
+	Token    string `json:"token"`
+	Username string `json:"username"`
+}
+type request struct {
+	Token string `json:"token"`
 }
 
 type User struct {
+	ID       int64  `json:"first"`
 	First    string `json:"first"`
 	Last     string `json:"last"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
 	Prof     string `json:"prof"`
+	Salt     string `json:"salt"`
 }
 type Users struct {
 	Users []User `json:"users"`
@@ -75,6 +85,25 @@ func StartServer() {
 
 }
 
+func redirectNonSecure(w http.ResponseWriter, r *http.Request) {
+	log.Println("Non-secure request initiated, redirecting.")
+	redirectURL := "https://" + serverName + r.RequestURI
+	// log.Println(redirectURL)
+	http.Redirect(w, r, redirectURL, http.StatusMovedPermanently)
+
+}
+func getToken(userid int64) string {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"uid": userid,
+		"iat": time.Now().Unix(),
+		"exp": time.Now().Add(time.Second * 3600 * 24).Unix(),
+	})
+	jwtToken, err := token.SignedString(secretKey)
+	if err != nil {
+
+	}
+	return jwtToken
+}
 func parsePost(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	decoder := json.NewDecoder(r.Body)
 
@@ -84,14 +113,18 @@ func parsePost(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	if err != nil {
 		fmt.Printf("Error occurred %s", err.Error())
 	}
-	fmt.Println(t.Username)
+	// fmt.Println(t.Username)
 	var user User
 	var response Response
-	database.QueryRow("Select password from Users where email='" + t.Username + "';").Scan(&user.Password)
-	fmt.Printf("\nThe username is: " + user.Password)
-	if t.Password == user.Password {
+	database.QueryRow("Select id,email,password,salt from Users where email='"+t.Username+"';").Scan(&user.ID, &user.Email, &user.Password, &user.Salt)
+	hash := Password.GenerateHash(user.Salt, t.Password)
+	if hash == user.Password {
+		fmt.Printf("\nThe username is: %s\n ", user.Email)
 		response.Message = "Correct Password"
-		response.Token = "1221ewadasa"
+		response.Token = getToken(user.ID)
+		fmt.Printf("The token is %s", response.Token)
+		response.Username = user.Email
+
 	} else {
 		response.Message = "Incorrect Password"
 		response.Token = ""
@@ -100,26 +133,57 @@ func parsePost(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, string(output))
 }
 
+func parseToken(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	log.Println("Checking Token")
+	var req request
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&req)
+	if err != nil {
+		fmt.Println("There was error : " + err.Error())
+	}
+	token, err := jwt.Parse(req.Token, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return secretKey, nil
+	})
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		fmt.Println(claims["uid"])
+	} else {
+		fmt.Println(err)
+	}
+	fmt.Fprintf(w, "HAHA")
+}
+
 func createUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	var newUser User
+	var getUser User
 	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(&newUser)
 	if err != nil {
 		fmt.Println("There was this error : " + err.Error())
 	}
 	var response Response
+	salt, password := Password.ReturnPassword(newUser.Password)
 	sql := "Insert into Users SET email='" +
 		newUser.Email +
 		"',first='" + newUser.First +
 		"',last='" + newUser.Last +
-		"',password='" + newUser.Password +
-		"',salt='we232d', prof='" + newUser.Prof + "';"
+		"',password='" + password +
+		"',salt='" + salt + "', prof='" + newUser.Prof + "';"
 	q, err := database.Exec(sql)
 	if err != nil {
 		fmt.Printf("Error creating user: %s", err.Error)
 		response.Message = err.Error()
+		response.Token = ""
 	} else {
+		database.QueryRow("Select id from Users where email='" + newUser.Email + "';").Scan(&getUser.ID)
 		response.Message = "Success"
+		response.Token = getToken(getUser.ID)
+		response.Username = newUser.Email
 	}
 	fmt.Println(q)
 	output, _ := json.Marshal(response)
@@ -132,10 +196,12 @@ func main() {
 	router := httprouter.New()
 	router.POST("/api/test", parsePost)
 	router.POST("/api/register", createUser)
+	router.POST("/api/token", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		fmt.Fprintf(w, "Hello,world")
+	})
 	router.GET("/api/view", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		fmt.Fprintf(w, "Hello,world")
 	})
 	handler := cors.Default().Handler(router)
 	log.Fatal(http.ListenAndServeTLS(":8080", "./cert.pem", "./key.pem", handler))
-	//fmt.Println("How are you?")
 }
